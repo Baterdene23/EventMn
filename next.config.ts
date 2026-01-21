c47f47f3a2b78027f122f6dafc4657a94b0b9c49
Ganbat Baterdene <22b1num0719@stud.num.edu.mn>
2026-01-21 12:46:52 +0800

    b

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sdyfhpyfsp9nwl46.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
